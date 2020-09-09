ecaeccde83d02cf41de64df82ff71e99414ed95b
RowenahWambui <suzygracy@gmail.com>
2020-09-09 23:00:20 +0300

    pause & play functions

---
+++ script.js
@@ -0,0 +1,25 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+let isPlaying = false;
+
+function playSong(){
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+function pauseSong(){
+    isPlaying = false;
+    prevBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+
+    music.pause();
+}
+
+
+
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
