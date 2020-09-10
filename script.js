273d2539f91bba24a35563ac4053308204a8baf8
RowenahWambui <suzygracy@gmail.com>
2020-09-10 10:37:25 +0300

    added the pevious & next functions, the progressbar update and current time display as audio plays

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeElement = document.getElementById('current-time');
+const durationElement = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+//Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'EDM',
+        artist: 'Jacinto'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Chill',
+        artist: 'Jacinto'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'POP',
+        artist: 'Jacinto'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'HIP',
+        artist: 'Jacinto'
+    }
+];
 
 let isPlaying = false;
 
@@ -13,13 +44,79 @@ function playSong(){
 }
 function pauseSong(){
     isPlaying = false;
-    prevBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.classList.replace('fa-pause', 'fa-play');
     playBtn.setAttribute('title', 'Play');
-
     music.pause();
 }
 
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`; 
+}
+
+//current song
+let songIndex = 0;
+
+//previous song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
 
 
+//next song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+loadSong(songs[songIndex]);
+
+function updateProgressBar(ev){
+    if(isPlaying){
+        const {duration , currentTime } = ev.srcElement;
+        const progressPercent = (currentTime / duration ) * 100;
+        progress.style.width = `${progressPercent}%`
+        //calculate duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if(durationSeconds < 10){
+            durationSeconds = `0${durationSeconds}`;
+        }
+        //delay
+        if(durationSeconds){
+            durationElement.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if(currentSeconds < 10){
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeElement.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+}
+
+function setProgressBar(ev) {
+    const width = this.clientWidth;
+    const clickX = ev.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration ;
+}
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
